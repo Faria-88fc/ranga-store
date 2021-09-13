836fd24a21db69b52af538f2c2e4d44bd0c0ae1a
faria88fc <faria-2019614566@psy.du.ac.bd>
2021-09-13 18:30:50 +0600

    bug fixed

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ const showProducts = (products) => {
       <p id="font">Category: ${product.category}</p>
       <p id="font">ratings: <span id="rating"> ${product.rating.rate}</span> <span id="vote">( total vote ${product.rating.count})</span></p>
       <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
+      <button onclick="addToCart(${product.id},${product.price},)" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
@@ -34,6 +34,7 @@ const addToCart = (id, price) => {
 
   updateTaxAndCharge();
   document.getElementById("total-Products").innerText = count;
+  updateTotal();
 };
 
 const getInputValue = (id) => {
@@ -47,12 +48,13 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = total; //Math.round(total);
+  document.getElementById(id).innerText = total.toFixed(2); //Math.round(total);
+  
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText =parseFloat(value).toFixed(2); // Math.round(value);
 };
 
 // update delivery charge and total Tax
@@ -77,5 +79,6 @@ const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
+
